59ec31387e7aa713e11b82d66f70bfa500bc54ce
Alan Magliolini <alannm54@gmail.com>
2024-03-14 02:07:52 -0300

    dia 2 terminado

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ export default function App() {
   const [ gifts, setGifts ] = useState( ["Vitel tone", "Auriculares", "Caramelos"] )
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center w-full">
-            <h1 className="text-5xl font-bold underline mb-4">Regalos:</h1>
+    <div className="flex flex-col h-screen items-center justify-center w-full ">
 
-            <ul>
-                 {gifts.map(gift => <li className="font-bold">{gift}</li>)}  
-            </ul>      
+          <div className="p-12 h-auto bg-white rounded-xl shadow-2xl">
+              <h1 className="text-5xl font-bold underline mb-4">Regalos:</h1>
+
+              <ul>
+                  {gifts.map(gift => <li className="font-bold">{gift}</li>)}  
+              </ul>  
+
+          </div>
+         
     </div>
    
   );
